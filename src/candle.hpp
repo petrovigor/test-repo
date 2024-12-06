fa2abb86c116bc36123a9ada1068cce381b5b397
SimpleFreak <ISFreak@yandex.ru>
2024-12-06 11:34:40 +0500

    Немного поменял начальную структуру проекта, изменил основной файл CMake.

---
+++ src/candle.hpp
@@ -0,0 +1,43 @@
+﻿#ifndef __CANDLE_HPP__
+#define __CANDLE_HPP__
+
+#include <cstdint>
+#include <string>
+
+using Price = double;
+
+struct Candle {
+public:
+    Price m_open;
+    Price m_high;
+    Price m_low;
+    Price m_close;
+
+public:
+    Candle() = default;
+    Candle(Price open, Price high, Price low, Price close);
+
+public:
+    /* находится ли цена внутри тела этой свечи */
+    bool bodyContains(const Price& price) const noexcept;
+
+    /* находится ли цена внутри тела и теней
+     * другими словами, касается ли цена
+     * тела или теней свечи
+     */
+    bool contains(const Price& price) const noexcept;
+
+    /* возвращает размер целой свечи, включая тени */
+    double fullSize() const noexcept;
+
+    /* возвращает размер тела свечи */
+    double bodySize() const noexcept;
+
+    /* Проверяет, является ли цвет зеленым */
+    bool isGreen() const noexcept;
+
+    /* Проверяет, является ли цвет красным */
+    bool isRed() const noexcept;
+};
+
+#endif
---
+++ src/tests.hpp
@@ -0,0 +1,6 @@
+#ifndef __TESTS_HPP__
+#define __TESTS_HPP__
+
+
+
+#endif
