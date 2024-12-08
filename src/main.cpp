250958bb9d97c85d617dc927cfab2da76d5df86a
SimpleFreak <ISFreak@yandex.ru>
2024-12-09 03:15:37 +0500

    Добавил библиотеку Boost, чтобы в перспективе использовать ее для написания тестов, удалил примерные варианты тестов

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,10 @@
 //массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
 
-//тест 1
-bool test1() {
-    //пример какого-то теста
-    return 42 == (41 + 1); //passed
-}
 
-//тест 2
-bool test2() {
-    //пример какого-то теста
-    return 42 == (41 + 1); //failed
-}
-
-//тест 3
-bool test3() {
-    Candle candle{ 0.0, 3.0, 3.0, 3.0 };
-
-    //пример какого-то теста
-    return candle.m_high == 3.0;
-}
 
 void initTests() {
-    tests.push_back(test1);
-    tests.push_back(test2);
-    tests.push_back(test3);
-    //tests.push_back(test4);
-    //tests.push_back(test5);
+
 }
 
 int launchTests() {
