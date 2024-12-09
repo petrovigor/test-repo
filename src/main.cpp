e0be6e0bc08bb99e443ca7a65f7226b8c5756ce5
SimpleFreak <ISFreak@yandex.ru>
2024-12-09 19:32:36 +0500

    Добавил тесты для пункта 2.2

--- src/main.cpp
+++ src/main.cpp
@@ -29,10 +29,40 @@ bool testBodyContainsRedBorder() {
     return candle.bodyContains(Price{10.0}) && candle.bodyContains(Price{20.0});
 }
 
+/* Проверка значения цены внутри тела свечи */
+bool testContainsInside() {
+    Candle candle{10.0, 20.0, 15.0, 18.0};
+
+    return candle.contains(Price{16.0}) &&
+           candle.contains(Price{15.0}) &&
+           candle.contains(Price{17.0}) &&
+           candle.contains(Price{18.0});
+}
+
+/* Проверка на значение цены вне диапазона */
+bool testContainsOutside() {
+    Candle candle{10.0, 20.0, 15.0, 18.0};
+
+    return !candle.contains(Price{22.0}) && !candle.contains(Price{9.0});
+}
+
+/* Проверка значения цена на границе диапазона */
+bool testContainsBorder() {
+    Candle candle{20.0, 15.0, 15.0, 20.0};
+
+    return candle.contains(Price{15.0}) && candle.contains(Price{15.0});
+}
+
 void initTests() {
+    /* Добавление тестов для метода bodyContains */
     tests.push_back(testBodyContainsGreenInside);
     tests.push_back(testBodyContainsRedInside);
     tests.push_back(testBodyContainsRedBorder);
+
+    /* Добавлениие тестов для метода contains */
+    tests.push_back(testContainsInside);
+    tests.push_back(testContainsOutside);
+    tests.push_back(testContainsBorder);
 }
 
 int launchTests() {
